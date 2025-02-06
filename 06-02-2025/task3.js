8fa5d84d33406caf708abad9701878643732c5f5
VishwavedKelkar <138898035+VishwavedKelkar@users.noreply.github.com>
2025-02-06 22:39:32 +0530

    Update task3.js

--- 06-02-2025/task3.js
+++ 06-02-2025/task3.js
@@ -4,43 +4,6 @@
 // Implement a cleanup mechanism to prevent memory issues.
 // Use Chrome DevTools to capture a memory snapshot and analyze retained objects to observe the simulated memory leak.
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const generateBtn = document.querySelector(".generate");
-//     const clearBtn = document.querySelector(".clear");
-
-//     let memoryArray = [];
-
-//     function addData() {
-//         for (let i = 0; i < 1000; i++) {
-//             memoryArray.push({
-//                 data: new Array(1000).fill("*"),
-//             });
-//         }
-//         console.log(`Array size: ${memoryArray.length}`);
-//     }
-
-//     let interval = null;
-
-//     generateBtn.addEventListener("click", () => {
-//         if (!interval) {
-//             interval = setInterval(addData, 1500);
-//         }
-//     });
-
-//     function cleanupMemory() {
-//         console.log(`Clearing array of size: ${memoryArray.length}`);
-//         clearInterval(interval);
-//         interval = null;
-//         memoryArray = [];
-//         console.log("Memory cleared! Array size:", memoryArray.length);
-//     }
-
-//     clearBtn.addEventListener("click", () => {
-//         cleanupMemory();
-//     });
-// });
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const generateBtn = document.querySelector(".generate");
     const clearBtn = document.querySelector(".clear");
